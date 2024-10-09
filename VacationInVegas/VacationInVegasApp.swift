791a8d31368553cc62f434d13f67e497cce7fec6
mnjammy007 <naseem4954r@gmail.com>
2024-10-09 16:00:12 +0530

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Apple on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VacationInVegasTests/VacationInVegasTests.swift
@@ -0,0 +1,16 @@
+//
+//  VacationInVegasTests.swift
+//  VacationInVegasTests
+//
+//  Created by Apple on 09/10/24.
+//
+
+import Testing
+
+struct VacationInVegasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
